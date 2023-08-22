20dd330405064aa82f78ef628b41f040996752f3
leloufadel <leloufadel@gmail.com>
2023-08-22 21:22:46 +0300

    updated Queries

--- queries.sql
+++ queries.sql
@@ -55,3 +55,47 @@ SELECT * FROM animals;
 ROLLBACK;
 --after ROLLBACK
 SELECT * FROM animals;
+
+-- Start a transaction
+BEGIN;
+
+-- Delete all animals born after Jan 1st, 2022
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint for the transaction
+SAVEPOINT sp1;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = -1 * weight_kg;
+
+-- Rollback to the savepoint
+ROLLBACK TO sp1;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = -1 * weight_kg
+WHERE weight_kg < 0;
+
+-- Commit transaction
+COMMIT;
+----------------------------------------------
+-- How many animals are there : 
+SELECT COUNT(*) AS total_Number_animals FROM animals;
+--How many animals have never tried to escape?
+SELECT COUNT(*) AS total_Neverescape FROM animals WHERE escape_attempts = 0;
+-- the average weight of animals.
+SELECT AVG (weight_kg) AS AVG_WEIGHT FROM animals;
+----
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts FROM animals GROUP BY neutered;
+--- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weigh FROM animals GROUP BY species;
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+Select species, AVG(escape_attempts) 
+FROM animals 
+where date_of_birth between '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+
+
+
