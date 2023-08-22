16e9e6ab0a6b9f5bf0ccff3b6e878b52d7ce1b04
leloufadel <leloufadel@gmail.com>
2023-08-22 11:00:13 +0300

    update data, queries, schema .sql with the project requirement

--- data.sql
+++ data.sql
@@ -4,18 +4,18 @@ INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
 
--- -- Animal: Agumon
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
--- VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
+-- Animal: Agumon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
 
--- -- Animal: Gabumon
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
--- VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
+-- Animal: Gabumon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
 
--- -- Animal: Pikachu
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
--- VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
+-- Animal: Pikachu
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
 
--- -- Animal: Devimon
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
--- VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
+-- Animal: Devimon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
